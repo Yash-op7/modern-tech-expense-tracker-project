21dd0d3317cd11d2ce6a3c958b62adc62dfd4872
yash-op7 <yashm3375@gmail.com>
2024-06-19 00:53:34 +0530

    set up router and all the tanstack stuff, working on ui

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {TanStackRouterVite} from '@tanstack/router-vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), TanStackRouterVite()],
   resolve: {
     alias: {
       "@" : path.resolve(import.meta.dir, "./src"),
@@ -14,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://127.0.0.1:3000",
         changeOrigin: true
       }
     }
