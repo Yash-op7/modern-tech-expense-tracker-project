e216b199b734770dd8d9f8516beacdd55e4d71fa
yash-op7 <yashm3375@gmail.com>
2024-06-19 15:16:42 +0530

    Create new expense form complete using tanstack form

--- frontend/src/routes/create-expense.tsx
+++ frontend/src/routes/create-expense.tsx
@@ -1,9 +1,87 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Label } from "@/components/ui/label"
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
-export const Route = createFileRoute ("/create-expense")({
+import { useForm } from '@tanstack/react-form'
+import type { FieldApi } from '@tanstack/react-form'
+
+export const Route = createFileRoute("/create-expense")({
   component: CreateExpense,
 })
 
 function CreateExpense() {
-  return <div className="p-2">Create Expense</div>
+  const form = useForm({
+    defaultValues: {
+      title: '',
+      amount: 0
+    },
+    onSubmit: async ({ value }) => {
+      console.log(value)
+    }
+  })
+
+  return (<div className="p-2">
+    <h2>Create Expense</h2>
+    {/* <form.Provider> */}
+      <form
+        className='max-w-xl w-full'
+        onSubmit={(e) => {
+          e.preventDefault()
+          e.stopPropagation()
+          void form.handleSubmit()
+        }}
+      >
+        <form.Field 
+          name="title" 
+          children={(field) => (
+          <>
+            <Label htmlFor={field.name}>Title</Label>
+            <Input
+              id={field.name}
+              name={field.name}
+              value={field.state.value}
+              onBlur={field.handleBlur}
+              onChange={(e) => field.handleChange(e.target.value)}
+            />
+            {field.state.meta.touchedErrors ? (
+              <em>{field.state.meta.touchedErrors}</em>
+            ) : null}
+          </>
+        )}
+        />
+        <form.Field 
+          name="amount" 
+          children={(field) => (
+          <>
+            <Label htmlFor={field.name}>Amount</Label>
+            <Input
+              id={field.name}
+              name={field.name}
+              value={field.state.value}
+              type="number"
+              onBlur={field.handleBlur}
+              onChange={(e) => field.handleChange(Number(e.target.value))}
+            />
+            {field.state.meta.touchedErrors ? (
+              <em>{field.state.meta.touchedErrors}</em>
+            ) : null}
+          </>
+        )}
+        />
+        <form.Subscribe
+            selector={(state) => [state.canSubmit, state.isSubmitting]}
+            children={([canSubmit, isSubmitting]) => (      // ! this will make the submit button unclickable untile the the onSubmit function in CreateExpense doesn't finish execution so that the form isn't submitted multiple times while the first time is being written to the db
+              <Button className='mt-4' type='submit' disabled={!canSubmit}>
+                {isSubmitting ? '...' : 'Submit'}
+              </Button>
+            )}
+          />
+
+        {/* <Button className='mt-4' type='submit'>Create Expense</Button> */}
+      </form>
+    {/* </form.Provider> */}
+  </div>
+  );
 }
+
